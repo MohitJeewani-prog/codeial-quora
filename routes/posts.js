6c6f48be377e7e34126773ac91c99bd285546220
Mohit Jiwani <mohitjiwani@gmail.com>
2019-09-29 20:02:17 +0530

    added a feature in ehich only registered and logged in users can post

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
-router.post('/create', postsController.create);
+//by adding passport.checkAuthantication, no user can edit html by creating
+//a form than posting a post
+router.post('/create', passport.checkAuthentication, postsController.create);
 
 module.exports = router;
